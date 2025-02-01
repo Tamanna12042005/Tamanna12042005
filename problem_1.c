9e3f581c60ce0132eb17c64f35254539bf4089c5
Tamanna12042005 <tamannageek2025@gmail.com>
2025-02-01 15:02:07 +0530

    Add files via upload

---
+++ problem_1.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int x;
+//     printf("enter any no.: ");
+//     scanf("%d",&x);
+//     if(x % 2 == 0){
+//         printf("%d is a even no.",x);
+//     }
+//     if(x % 2 != 0){
+//         printf("%d is an odd no.",x);
+//     }
+//     return 0;
+// }
